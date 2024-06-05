2b4809a2156f293123b91ac8b11346dd63c90775
pozharskye <evgeny.balashov.w@gmail.com>
2024-06-05 03:06:52 +0300

    .

--- servers-setup/semi-automated/common-part2.sh
+++ servers-setup/semi-automated/common-part2.sh
@@ -7,29 +7,30 @@ if (( $EUID != 0 )); then
 fi
 
 
-# ---------- Docker installation ---------------
-# Add Docker's official GPG key:
-apt-get update
-apt-get install ca-certificates curl
-install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
-chmod a+r /etc/apt/keyrings/docker.asc
-
-# Add the repository to Apt sources:
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt-get update
-
-
-apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
-service docker start
+# ---------- Install CRI (CRI-O) ---------------
+apt-get update -qq && apt-get install -y \
+    libbtrfs-dev \
+    containers-common \
+    git \
+    libassuan-dev \
+    libglib2.0-dev \
+    libc6-dev \
+    libgpgme-dev \
+    libgpg-error-dev \
+    libseccomp-dev \
+    libsystemd-dev \
+    libselinux1-dev \
+    pkg-config \
+    go-md2man \
+    cri-o-runc \
+    libudev-dev \
+    software-properties-common \
+    gcc \
+    make
 # --------------------------------------------------
 
 
-# --- Installing kubeadm, kubelet and kubectl -----
+# ----- Installing kubeadm, kubelet and kubectl ------
 apt-get install -y apt-transport-https ca-certificates curl gpg
 
 
--- servers-setup/semi-automated/master.sh
+++ servers-setup/semi-automated/master.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
+# Check if user is root or not
+if (( $EUID != 0 )); then
+    echo "PLEASE, LOGIN AS ROOT BEFORE RUNNING THIS SCRIPT!"
+    exit
+fi
+
+
 # ------ Initializing master node ---------------
 kubeadm init
 
 export KUBECONFIG=/etc/kubernetes/admin.conf
+
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+# -----------------------------------------
