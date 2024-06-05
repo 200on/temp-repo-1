da89f591b98661acb45fc2ea2f59cdd31ed7c77d
pozharskye <evgeny.balashov.w@gmail.com>
2024-06-05 15:39:59 +0300

    .

--- servers-setup/cloned-and-edited/master.sh
+++ servers-setup/cloned-and-edited/master.sh
@@ -2,7 +2,6 @@
 #
 # Setup for Control Plane (Master) servers
 
-apt-get install sudo
 
 # If you need public access to API server using the servers Public IP adress, change PUBLIC_IP_ACCESS to true.
 
@@ -18,7 +17,7 @@ sudo kubeadm config images pull
 
 if [[ "$PUBLIC_IP_ACCESS" == "false" ]]; then
     
-    MASTER_PRIVATE_IP=$(ip addr show eth0 | awk '/inet / {print $2}' | cut -d/ -f1)
+    MASTER_PRIVATE_IP=$(ip addr show lo | awk '/inet / {print $2}' | cut -d/ -f1)
     sudo kubeadm init --apiserver-advertise-address="$MASTER_PRIVATE_IP" --apiserver-cert-extra-sans="$MASTER_PRIVATE_IP" --pod-network-cidr="$POD_CIDR" --node-name "$NODENAME" --ignore-preflight-errors Swap
 
 elif [[ "$PUBLIC_IP_ACCESS" == "true" ]]; then
